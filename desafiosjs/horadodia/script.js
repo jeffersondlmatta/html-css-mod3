267aa60da0a1e57bce70842e8f295fbb7acc0909
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2022-04-27 14:49:21 -0300

    ok

--- desafiosjs/horadodia/script.js
+++ desafiosjs/horadodia/script.js
@@ -1,1 +1,20 @@
-alert('ola')
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 22 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'manha.jpg'
+        document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE !
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'orange'
+    } else {
+        //BOA NOITE!
+        img.src = 'noite.jpg'
+        document.body.style.background = 'gray'
+    }
+}
