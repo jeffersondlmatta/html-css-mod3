08af5ee077355167a539fc49ff016ec25d6f34e2
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2022-04-28 13:49:03 -0300

    ok

--- desafiosjs/buscadepessoas/script.js
+++ desafiosjs/buscadepessoas/script.js
@@ -16,27 +16,31 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10 ) {
                 //criança 
-                img.setAttribute('src', 'foto-bebemas.jpg')               
+                img.setAttribute('src', 'foto-bebe-m.png')               
             } else if (idade < 21 ) {
                 //Jovem
-                img.setAttribute('src', 'foto-jovemmas.jpg')
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50 ) {
                 //Adulto
-                img.setAttribute('src', 'foto-adultomas.jpg')
+                img.setAttribute('src', 'foto-adulto-m.png')
             } else {
                 //Idoso
-                img.setAttribute('src', 'foto-idosomas.jpg')
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10 ) {
-                //criança                
+                //criança 
+                img.setAttribute('src', 'foto-bebe-f.png')               
             } else if (idade < 21 ) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
             } else if (idade < 50 ) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
             }
         }
         res.style.textAlign = 'center'
