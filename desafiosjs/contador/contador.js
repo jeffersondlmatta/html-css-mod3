5229377a6c1f44bf5b4e0eef23467173f5db7487
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2022-05-10 12:52:12 -0300

    concluido

--- desafiosjs/contador/contador.js
+++ desafiosjs/contador/contador.js
@@ -7,6 +7,24 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
         window.alert('[ERRO] Verifique os dados! ') 
     } else {
-        
-    }
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            p = 1
+        }
+        if (i < f ) {
+            //contagem crescente
+            for(let c = i; c<=f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }               
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+             res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }   
 }
