c9bc9714947b7ef28176801391d9b5826524e0a2
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2022-05-13 11:22:19 -0300

    up

---
+++ desafiosjs/taboada/script.js
@@ -0,0 +1,9 @@
+function tabuada() {    
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0 ) {
+        window.alert('Dados invalidos')        
+    } else {
+        let n = Number(num.value)
+    }
+}
